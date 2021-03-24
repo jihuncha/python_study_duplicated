4365b8ab3ffdeabbc433107a9627209f6649a2ef
차지훈 <jihun.cha@tisquare.com>
2021-03-24 15:14:38 +0900

    test_2

---
+++ pycharm_folder/210324_book_leetcode/palindrome.py
@@ -0,0 +1,58 @@
+# 팰린드롬 = 회문
+# 거꾸로 읽어도 똑같은
+
+#리스트 변환처리
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         strs = []
+#
+#         for char in s:
+#             # 숫자와 영어만 인지
+#             if char.isalnum():
+#                 strs.append(char.lower())
+#
+#         while len(strs) > 1:
+#             if strs.pop(0) != strs.pop():
+#                 return False
+#
+#         return True
+
+#deque 이용하여 속도 개선
+from collections import deque
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # strs = []
+
+        strs = deque()
+        # strs: Deque = collections.deque()
+
+        for char in s:
+            # 숫자와 영어만 인지
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
+
+# 슬라이싱
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]
+
+print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
+
+
+
+
+
+
---
+++ pycharm_folder/210324_book_leetcode/reverse_string.py
@@ -0,0 +1,42 @@
+from typing import List
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#
+#         temp_list = []
+#         for i in range(len(s)):
+#             temp_list.append(s[i])
+#         # index
+#         left, right = 0, len(temp_list) - 1
+#         while left < right:
+#             temp_list[left], temp_list[right] = temp_list[right], temp_list[left]
+#             left += 1
+#             right -= 1
+#
+#         print(temp_list)
+# print(Solution().reverseString("hello"))
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # return s[::-1]
+        # 풀이 1
+        s.reverse()
+        # 풀이 2
+        # 공간복잡도 제약에 의해 하기와같이 표현해야 사용가능
+        s[:] = s[::-1]
+
+print(Solution().reverseString("hello"))
+
+# temp_list = [1,2,3]
+# second_list = [4,5,6]
+#
+# for check in range(3):
+#     temp_list[check],  second_list[check] = second_list[check], temp_list[check]
+#
+# print(temp_list, second_list)

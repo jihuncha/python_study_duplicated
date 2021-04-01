33ff0d07ad09f11cc58458219a25400615e72f67
차지훈 <jihun.cha@tisquare.com>
2021-04-01 17:50:44 +0900

    d

--- pycharm_folder/210325_book_string_manipulation/210330_again.py
+++ pycharm_folder/210325_book_string_manipulation/210330_again.py
@@ -145,4 +145,42 @@ from typing import Deque, List, re
 #     def longestPalindrome(self, s: str) -> str:
 #         collections.deque =
 
+# Input: s = "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
 
+# s = "babad"
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         m = ''
+#         for i in range(len(s)):
+#             for j in range(len(s), i,-1):
+#                 if len(m) >= j - i:  # To reduce time
+#                     break
+#                 elif s[i:j] == s[i:j][::-1]:
+#                     m = s[i:j]
+#                     break
+#         return m
+#
+# print(Solution().longestPalindrome(s))
+
+s = "babad"
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left : int, right: int) -> str:
+            print("left/right - {},{}".format(left,right))
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        if len(s) < 2 or s == s[::-1]:
+            return s
+
+        result = ''
+        for i in range(len(s) - 1):
+            result = max(result, expand(i, i+1), expand(i, i+2), key=len)
+
+        return result
+
+print(Solution().longestPalindrome(s))

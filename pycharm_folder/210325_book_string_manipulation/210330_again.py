b980951c9454827fc0ad457b937aa7dd30a3e274
차지훈 <jihun.cha@tisquare.com>
2021-04-05 16:31:12 +0900

    개졸림

--- pycharm_folder/210325_book_string_manipulation/210330_again.py
+++ pycharm_folder/210325_book_string_manipulation/210330_again.py
@@ -194,46 +194,252 @@ from typing import Deque, List, re
 height = [4,2,0,3,2,5]
 # Output: 9
 
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        stack = []
-        volume = 0
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         stack = []
+#         volume = 0
+#
+#         for i in range(len(height)):
+#             while stack and height[i] > height[stack[-1]]:
+#                 top = stack.pop()
+#
+#                 if not len(stack):
+#                     break
+#
+#                 # ??? 이전과의 차이만큼 물 높이 처리?
+#                 distance = i - stack[-1] -1
+#                 waters = min(height[i], height[stack[-1]]) - height[top]
+#
+#                 volume += distance * waters
+#
+#             stack.append(i)
+#         return volume
+#
+#
+#         # if not height:
+#         #     return 0
+#         #
+#         # left, right = 0, len(height) - 1
+#         # left_max, right_max = height[left], height[right]
+#         # volume = 0
+#         #
+#         # while left < right:
+#         #     if left_max < right_max:
+#         #         left += 1
+#         #         if left_max - height[left] > 0:
+#         #             volume += left_max - height[left]
+#         #         left_max = max(left_max, height[left])
+#         #     else :
+#         #         right -= 1
+#         #         if right_max - height[right] > 0:
+#         #             volume += right_max - height[right]
+#         #         right_max = max(right_max, height[right])
+#         #
+#         # return volume
+#
+# print(Solution().trap(height))
 
-        for i in range(len(height)):
-            while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
+# Example 1:
+#
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Example 2:
+#
+# Input: nums = []
+# Output: []
+# Example 3:
+#
+# Input: nums = [0]
+# Output: []
 
-            if not len(stack):
-                break
+nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
 
-            # ??? 이전과의 차이만큼 물 높이 처리?
-            distance = i - stack[-1] -1
-            waters = min(height[i], height[stack[-1]]) - height[top]
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         nums.sort()
+#
+#         for i in range(len(nums) - 2):
+#
+#             # 중복값 건너 뛰기
+#             if i > 0 and nums[i] == nums[i-1]:
+#                 continue
+#
+#             left, right = i + 1, len(nums) - 1
+#
+#             while left < right:
+#                 sum_all = nums[i] + nums[left] + nums[right]
+#
+#                 if sum_all < 0:
+#                     left +=1
+#                 elif sum_all > 0:
+#                     right -=1
+#                 else:
+#                     result.append([nums[i],nums[left],nums[right]])
+#
+#                     # 같은 숫자만큼 이동
+#                     while left < right and nums[left] == nums[left + 1]:
+#                         left +=1
+#                     while left < right and nums[right] == nums[right - 1]:
+#                         right -=1
+#
+#                     left +=1
+#                     right-=1
+#
+#         return result
 
-            volume += distance * waters
+# print(Solution().threeSum(nums))
 
-        stack.append(i)
+# nums = [-1,1,0,-3,3]
+#
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         result = []
+#         p = 1
+#
+#         for i in range(0,len(nums)):
+#             result.append(p)
+#             p = p * nums[i]
+#
+#         p = 1
+#
+#         for i in range(len(nums) - 1, 0 - 1, -1):
+#             result[i] = result[i] * p
+#             p = p * nums[i]
+#
+#         return result
+#
+# print(Solution().productExceptSelf(nums))
 
 
-        # if not height:
-        #     return 0
-        #
-        # left, right = 0, len(height) - 1
-        # left_max, right_max = height[left], height[right]
-        # volume = 0
-        #
-        # while left < right:
-        #     if left_max < right_max:
-        #         left += 1
-        #         if left_max - height[left] > 0:
-        #             volume += left_max - height[left]
-        #         left_max = max(left_max, height[left])
-        #     else :
-        #         right -= 1
-        #         if right_max - height[right] > 0:
-        #             volume += right_max - height[right]
-        #         right_max = max(right_max, height[right])
-        #
-        # return volume
 
-print(Solution().trap(height))
+# 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+# 2 초	128 MB	68947	35835	30407	52.057%
+# 문제
+# 어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다.
+# 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+# 입력
+# 첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+#
+# 출력
+# 첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+
+# 예제 입력 1
+# 110
+# 예제 출력 1
+# 99
+# 예제 입력 2
+# 1
+# 예제 출력 2
+# 1
+# 예제 입력 3
+# 210
+# 예제 출력 3
+# 105
+# 예제 입력 4
+# 1000
+# 예제 출력 4
+# 144
+
+# import sys
+# data = int(sys.stdin.readline())
+#
+# count = 0
+# # print(i)
+# for check in range(1, data + 1):
+#     temp_data = str(check)
+#     if len(temp_data) == 4:
+#         break
+#     elif len(temp_data) <= 2:
+#         count += 1
+#     else:
+#         temp_data = str(check)
+#         if int(temp_data[0]) - int(temp_data[1]) == int(temp_data[1]) - int(temp_data[2]):
+#             count+=1
+#
+# print(count)
+
+# 문제
+# 카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 있다.
+#
+# 한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+#
+# 김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+#
+# 이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+#
+# N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+#
+# 입력
+# 첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+#
+# 합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+#
+# 출력
+# 첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+
+# 예제 입력 1
+# 5 21
+# 5 6 7 8 9
+# 예제 출력 1
+# 21
+# 예제 입력 2
+# 10 500
+# 93 181 245 214 315 36 185 138 216 295
+# 예제 출력 2
+# 497
+
+# import sys
+# n, m = map(int,sys.stdin.readline().split())
+#
+# data = list(map(int, sys.stdin.readline().split()))
+#
+# # print(n,m)
+# # print(data)
+#
+# result = []
+# for i in range(n):
+#     for j in range(0, i):
+#         for k in range(i, n):
+#             if i == j or i == k or j == k:
+#                 continue
+#             temp_result = data[i] + data[j] + data[k]
+#             if temp_result == m:
+#                 result.append(temp_result)
+#                 break
+#
+#             if data[i] + data[j] + data[k] < m:
+#                 result.append(data[i] + data[j] + data[k])
+#
+# print(max(result))
+
+# 문제
+# 어떤 자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다.
+# 어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다. 예를 들어, 245의 분해합은 256(=245+2+4+5)이 된다.
+# 따라서 245는 256의 생성자가 된다. 물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다. 반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
+#
+# 자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 자연수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+#
+# 출력
+# 첫째 줄에 답을 출력한다. 생성자가 없는 경우에는 0을 출력한다.
+
+# 예제 입력 1
+# 216
+# 예제 출력 1
+# 198
+
+m = int(input())
+
+result = []
+for i in range(1, m+1):
+    temp = list(map(int, str(i)))
+    temp_result = i + sum(temp)
+    if temp_result == m:
+        print(i)
+
+    if i == m:
+        print(0)

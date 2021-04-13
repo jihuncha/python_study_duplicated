304c2bebb9265f1fbf57727b38959d4725230e7f
차지훈 <jihun.cha@tisquare.com>
2021-04-13 17:04:03 +0900

    jkk

---
+++ pycharm_folder/practice/210413_back/2178.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2178
+
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+
+print(n,m)
+
+data_list = [[(list(input()))] * m for _ in range(n)]
+
+
+
+
+print(data_list)
+
+

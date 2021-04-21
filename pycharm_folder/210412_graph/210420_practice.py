ff2b9595fb39d3d514850f968374697ee1e04cb4
차지훈 <jihun.cha@tisquare.com>
2021-04-21 20:00:47 +0900

    ㅌㄱ

--- pycharm_folder/210412_graph/210420_practice.py
+++ pycharm_folder/210412_graph/210420_practice.py
@@ -17,4 +17,47 @@ nums = [1,2,3]
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+        dfs(nums)
+        return results
+
+print(Solution().permute(nums))
+
+# Example 1:
+#
+n = 4
+k = 2
+# Output:
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ]
+# Example 2:
+#
+# Input: n = 1, k = 1
+# Output: [[1]]
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+
+
 
